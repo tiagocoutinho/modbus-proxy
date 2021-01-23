bf50b81d02c6e5149701c5f5c1f696a73527d3db
Jose Tiago Macara Coutinho <coutinhotiago@gmail.com>
2021-01-23 19:33:11 +0100

    Fix minimum python version to 3.7

--- setup.py
+++ setup.py
@@ -28,15 +28,13 @@ test_requirements = ['pytest>=3', ]
 setup(
     author="Tiago Coutinho",
     author_email='coutinhotiago@gmail.com',
-    python_requires='>=3.5',
+    python_requires='>=3.7',
     classifiers=[
         'Development Status :: 2 - Pre-Alpha',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
         'Natural Language :: English',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
