36653cd49c16b1312d13c2fbd966d962a21762b4
Jose Tiago Macara Coutinho <coutinhotiago@gmail.com>
2021-01-25 15:44:56 +0100

    More robust server
    * when attempting to communicate with slave make a second attempt in case the socket was closed by the slave
    * close the connection to the client if any error occurs
    * handle clients which close sockets after each request
    * make sure each message on both ends are full TCP messages (sometimes TCP packets can be broken into smaller pieces so we need to make sure we read a complete modbus message before sending it to the other side)
    * add a modbus timeout
    * better logging

--- modbus_proxy.py
+++ modbus_proxy.py
@@ -8,15 +8,13 @@ __version__ = "0.2.0"
 log = logging.getLogger("modbus-proxy")
 
 
-class ConnectionClosedError(ConnectionError):
-    pass
-
-
 class Connection:
 
-    def __init__(self, reader, writer):
+    def __init__(self, name, reader, writer):
+        self.name = name
         self.reader = reader
         self.writer = writer
+        self.log = log.getChild(name)
 
     async def __aenter__(self):
         return self
@@ -30,117 +28,140 @@ class Connection:
 
     async def close(self):
         if self.writer is not None:
-            self.writer.close()
-            await self.writer.wait_closed()
-            self.reader = None
-            self.writer = None
+            self.log.info("closing connection...")
+            try:
+                self.writer.close()
+                await self.writer.wait_closed()
+            except Exception as error:
+                self.log.info("failed to close: %r", error)
+            else:
+                self.log.info("connection closed")
+            finally:
+                self.reader = None
+                self.writer = None
 
-    async def write(self, data):
+    async def _write(self, data):
+        self.log.debug("sending %r", data)
         self.writer.write(data)
         await self.writer.drain()
 
-    async def read(self):
-        # TODO: make sure packet is complete
-        reply = await self.reader.read(8192)
-        if not reply:
-            raise ConnectionClosedError("disconnected")
+    async def write(self, data):
+        try:
+            await self._write(data)
+        except Exception as error:
+            self.log.error("writting error: %r", error)
+            await self.close()
+            return False
+        return True
+
+    async def _read(self):
+        """Read ModBus TCP message"""
+        # TODO: Handle Modbus RTU and ASCII
+        header = await self.reader.readexactly(6)
+        size = int.from_bytes(header[4:], "big")
+        reply = header + await self.reader.readexactly(size)
+        self.log.debug("received %r", reply)
         return reply
 
-
-class ModBus(Connection):
-
-    def __init__(self, host, port):
-        self.host = host
-        self.port = port
-        self.log = log.getChild(f"ModBus({host}:{port})")
-        super().__init__(None, None)
-
-    async def open(self):
-        await self.close()
-        self.log.info("connecting to modbus at %s...", (self.host, self.port))
-        self.reader, self.writer = \
-            await asyncio.open_connection(self.host, self.port)
-        self.log.info("connected!")
+    async def read(self):
+        try:
+            return await self._read()
+        except asyncio.IncompleteReadError as error:
+            if error.partial:
+                self.log.error("reading error: %r", error)
+            else:
+                self.log.info("client closed connection")
+            await self.close()
+        except Exception as error:
+            self.log.error("reading error: %r", error)
+            await self.close()
 
 
 class Client(Connection):
 
     def __init__(self, reader, writer):
-        super().__init__(reader, writer)
         peer = writer.get_extra_info("peername")
-        self.log = log.getChild(f'Client({peer[0]}:{peer[1]})')
+        super().__init__(f"Client({peer[0]}:{peer[1]})", reader, writer)
+        self.log.info("new client connection")
 
 
-class Server:
+class ModBus(Connection):
 
-    def __init__(self, host, port, modbus, timeout=None):
+    def __init__(self, host, port, modbus_host, modbus_port, timeout=None):
+        super().__init__(f"ModBus({modbus_host}:{modbus_port})", None, None)
         self.host = host
         self.port = port
-        self.modbus = modbus
+        self.modbus_host = modbus_host
+        self.modbus_port = modbus_port
         self.timeout = timeout
         self.lock = asyncio.Lock()
 
-    async def ensure_modbus_connection(self):
-        async with self.lock:
-            if not self.modbus.opened:
-                await asyncio.wait_for(self.modbus.open(), self.timeout)
+    async def open(self):
+        self.log.info("connecting to modbus...")
+        self.reader, self.writer = \
+            await asyncio.open_connection(self.modbus_host, self.modbus_port)
+        self.log.info("connected!")
 
-    async def modbus_write_read(self, data):
+    async def write_read(self, data, attempts=2):
         async with self.lock:
-            return await asyncio.wait_for(
-                self._modbus_write_read(data), self.timeout
-            )
+            for i in range(attempts):
+                try:
+                    if not self.opened:
+                        await asyncio.wait_for(self.open(), self.timeout)
+                    coro = self._write_read(data)
+                    return await asyncio.wait_for(coro, self.timeout)
+                except Exception as error:
+                    self.log.error("write_read error: %r", error)
+                    await self.close()
 
-    async def _modbus_write_read(self, data):
-        await self.modbus.write(data)
-        return await self.modbus.read()
+    async def _write_read(self, data):
+        await self._write(data)
+        return await self._read()
 
     async def handle_client(self, reader, writer):
         async with Client(reader, writer) as client:
-            log = client.log
-            log.info('new connection')
-            try:
-                while True:
-                    await self.ensure_modbus_connection()
-                    request = await client.read()
-                    await self.ensure_modbus_connection()
-                    log.debug("processing client to modbus")
-                    reply = await self.modbus_write_read(request)
-                    log.debug("processing reply from modbus to client")
-                    await client.write(reply)
-            except ConnectionClosedError as closed:
-                log.info("%r", closed)
-            except Exception as error:
-                log.error("%r", error)
+            while True:
+                request = await client.read()
+                if not request:
+                    return
+                reply = await self.write_read(request)
+                if not reply:
+                    return
+                result = await client.write(reply)
+                if not result:
+                    return
 
     async def serve_forever(self):
         server = await asyncio.start_server(
             self.handle_client, self.host, self.port
         )
         async with server:
+            self.log.info("Ready to accept requests on %s:%d", self.host, self.port)
             await server.serve_forever()
 
 
 async def run(server_url, modbus_url, timeout):
-    async with ModBus(modbus_url.hostname, modbus_url.port) as modbus:
-        server = Server(server_url.hostname, server_url.port, modbus, timeout)
-        await server.serve_forever()
+    async with ModBus(server_url.hostname, server_url.port, modbus_url.hostname, modbus_url.port, timeout) as modbus:
+        await modbus.serve_forever()
 
 
 def main():
-    parser = argparse.ArgumentParser(description="ModBus proxy")
+    parser = argparse.ArgumentParser(
+        description="ModBus proxy",
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter
+    )
     parser.add_argument(
         "-b", "--bind", type=urlparse, default="tcp://0:5020",
-        help="listen address (ex: tcp://0:502)"
+        help="listen address"
     )
     parser.add_argument("--modbus", type=urlparse,
         help="modbus device address (ex: tcp://plc.acme.org:502)"
     )
-    parser.add_argument("--timeout", type=float, default=None,
-        help="modbus connection and request timeout (s)"
+    parser.add_argument("--timeout", type=float, default=10,
+        help="modbus connection and request timeout in seconds"
     )
-    parser.add_argument("--log-level", default='INFO',
-        choices=['DEBUG', 'INFO', 'WARNING', 'ERROR'],
+    parser.add_argument("--log-level", default="INFO",
+        choices=["DEBUG", "INFO", "WARNING", "ERROR"],
         help="modbus connection and request timeout (s)"
     )
     args = parser.parse_args()
