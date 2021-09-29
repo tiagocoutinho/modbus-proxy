db2576d821325802c0da8a26ca6a9212af04ade4
Jose Tiago Macara Coutinho <coutinhotiago@gmail.com>
2021-09-29 11:19:25 +0200

    Move to setup.cfg

--- setup.py
+++ setup.py
@@ -9,56 +9,5 @@
 
 from setuptools import setup
 
-with open('README.md') as readme_file:
-    readme = readme_file.read()
-
-with open('HISTORY.md') as history_file:
-    history = history_file.read()
-
-setup_requirements = ['pytest-runner', ]
-
-test_requirements = ['pytest>=3', ]
-
-extras_require = {
-    "yaml": ["PyYAML"],
-    "toml": ["toml"],
-}
-
-setup(
-    author="Tiago Coutinho",
-    author_email='coutinhotiago@gmail.com',
-    python_requires='>=3.7',
-    classifiers=[
-        'Development Status :: 4 - Beta',
-        'Intended Audience :: Developers',
-        'Intended Audience :: Manufacturing',
-        'Intended Audience :: Science/Research',
-        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
-        'Natural Language :: English',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
-        'Programming Language :: Python :: 3.9',
-    ],
-    description="ModBus TCP proxy",
-    entry_points={
-        "console_scripts": [
-            'modbus-proxy=modbus_proxy:main',
-        ],
-    },
-    license="GNU General Public License v3",
-    long_description=readme + '\n\n' + history,
-    long_description_content_type="text/markdown",
-    include_package_data=True,
-    keywords='modbus, tcp, proxy',
-    name='modbus-proxy',
-    package_dir={'': 'src'},
-    py_modules=['modbus_proxy'],
-    extras_require=extras_require,
-    setup_requires=setup_requirements,
-    test_suite='tests',
-    tests_require=test_requirements,
-    url='https://github.com/tiagocoutinho/modbus-proxy',
-    version='0.5.0',
-    zip_safe=False,
-)
+if __name__ == "__main__":
+    setup()
